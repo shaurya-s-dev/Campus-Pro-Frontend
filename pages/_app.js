7bb7d25bfce3681c2cb362ed6226ba88a02d6884
Shaurya Singh <shauryasingh8d@gmail.com>
2026-03-18 21:50:49 +0530

    Wrap Component with Head for meta tags
    Added Head component for SEO and performance enhancements.

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,16 @@
 import "@/styles/globals.css";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="theme-color" content="#04040b" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
